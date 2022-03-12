5bbccfba22dfd5ddc2186400846a045122bf2dcb
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2022-03-12 18:17:04 -0300

    feat: retirando pacotes desnecessarios

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,9 @@
-import { ApolloProvider } from '@apollo/client';
-import client from '../utils/client';
 import '../styles/fonts.css';
 import '../styles/global.css';
 import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
